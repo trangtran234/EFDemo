783a4460d2a58e96b0be864fbf2c2d7c668883ef
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-04-12 18:04:48 +0700

    the first commit: create connection with sql server to apply database first

--- EFDemo/Program.cs
+++ EFDemo/Program.cs
@@ -10,6 +10,25 @@ namespace EFDemo
     {
         static void Main(string[] args)
         {
+            using (var db = new MoviesManagementEntities())
+            {
+                var actors = (from a in db.ACTORs
+                              join c in db.CASTS on a.id equals c.mid
+                              join m in db.MOVIEs on c.pid equals m.id
+                              where m.name == "Officer 444"
+                              select new
+                              {
+                                  firstName = a.fname,
+                                  lastName = a.lname
+                              }).ToList();
+                Console.WriteLine("Console here");
+                foreach (var item in actors)
+                {
+                    Console.WriteLine("First Name: " + item.firstName);
+                    Console.WriteLine("Last Name: " + item.lastName);
+
+                }
+            }
         }
     }
 }
